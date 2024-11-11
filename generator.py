46c86a80a253d7d76b4cc40f0435b2ec3f1f8d05
JackDevilGod <151947211+JackDevilGod@users.noreply.github.com>
2024-11-11 23:00:18 +0100

    made comment plans

---
+++ generator.py
@@ -0,0 +1,27 @@
+from copy import deepcopy
+
+
+def main():
+    # init first node
+
+    # init node storage with first node
+
+    # init working list with first node
+
+    while True:
+        pass
+        # for loop over working list
+            # create both nodes stemming from the og node
+
+            # iterate over the one or two nodes
+                # check if the nodes are in storage
+                    # update the nodes in storage
+                # else
+                    # add the node into storage
+                    # add to plan to work list
+
+        # update working list
+
+
+if __name__ == '__main__':
+    main()
