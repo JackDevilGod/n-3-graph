83c3a29658133f2919a8bff2ece88430c4c9922c
JackDevilGod <151947211+JackDevilGod@users.noreply.github.com>
2024-11-15 22:29:45 +0100

    added str and repr representation of node

--- classes/node.py
+++ classes/node.py
@@ -18,9 +18,19 @@ class NumNode:
         """
         self._from.add(value)
 
+    def is_in_from(self, node: "NumNode") -> bool:
+        return (node.value in self._from)
+
     def __eq__(self, value: "NumNode") -> bool:
+
         return (self.value == value.value)
 
     def dict(self) -> dict:
         return {"value": self.value,
                 "from": list(self._from)}
+
+    def __str__(self) -> str:
+        return f'{self.value}'
+
+    def __repr__(self) -> str:
+        return f"{self.value}"
